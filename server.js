b8a7f2ab4a1d643a02009ee605237cb5180d5424
Joao Paulo Paz <jppaztech@gmail.com>
2026-04-24 01:03:48 +0000

    aula finalizada 23/04

--- server.js
+++ server.js
@@ -2,11 +2,31 @@ const http = require("http")
 const fs = require("fs")
 
 const server = http.createServer((req, res) => {
-   const file = fs.readFileSync("index.html")
+    console.log(req.url)
+
+    file = ""
+    if(req.url === "/react"){
+        file = fs.readFileSync("reactjs/index.html")
+    }
+    
+    if(req.url === "/angular"){
+        file = fs.readFileSync("angularjs/index.html")
+
+    }
+
+    if(req.url === "/"){
+        file = fs.readFileSync("index.html")
+
+        file = file.toString().replace("{{title}}", "Estou na HOME VELHO!!!")
+
+    }
+   const fileindex = fs.readFileSync("index.html")
    const fileVanilla = fs.readFileSync("vanillajs/index.html")
    const fileJquery = fs.readFileSync("jqueryjs/index.html")
    const angularjs = fs.readFileSync("angularjs/index.html")
-    res.end(angularjs)
+   const reactjs = fs.readFileSync("reactjs/index.html")
+   const vuejs = fs.readFileSync("vuejs/index.html")
+    res.end(file)
 })
 
 server.listen(3000, () => { console.log("No ar 3000") })
